a83b091415b84eb4f3afa62a23a33a137af75fb3
Thalles silva <thalles.silva@mercadolivre.com>
2025-02-15 13:37:13 -0300

    feat: config initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  test: {
+    environment: 'jsdom',
+    setupFiles: '_tests_/setup.ts',
+  },
+  server: {
+    port: 3000,
+  }
 })
